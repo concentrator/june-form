6465631dd7cd7bb7be339f7244dafa4c8f8354ee
Dmitry Iovlev <iovlev.d@gmail.com>
2019-04-25 15:34:32 +0300

    Adds js fixes

--- src/js/script.js
+++ src/js/script.js
@@ -29,18 +29,23 @@
     activeResult: null,
     clickFromResult: false,
     errorMessage: null,
+    isStorageSupport: true,
+    storage: '',
 
     nextButton: app.querySelector('.js-btn-next'),
 
     nextButtonClickHandler: function (evt) {
-
       evt.preventDefault();
 
       // Current Section Index
       var i = June.currentIndex;
 
       // If validation passed get Current Section input with value
-      var el = June.validateInputs(sections[i]);
+      // Each section has to contain ony 1 field
+      var inputs = sections[i].querySelectorAll('.js-input');
+      var el = June.validateInputs(inputs);
+
+      June.setStorageValue(el);
 
       if (el) {
         var inputObj = June.makeResultObject(el);
@@ -91,43 +96,56 @@
     getErrorMsg: function () {
       if(!this.errorMessage) {
         var el = document.createElement('p');
-        el.style.color = 'red';
-        el.textContent = 'Please, select something';
+        el.textContent = 'Please, fill all fields and select options';
         el.classList.add('form__error');
         this.errorMessage = el;
       }
       return this.errorMessage;
     },
 
-    showNavigation: function() {
+    showNavigation: function () {
       appNavigation.classList.remove('u-hidden');
       appNavigation.classList.add('fade-in');
     },
 
-    validateInputs: function(section) {
+    validateInputs: function (inputs) {
       var errorMessage = this.getErrorMsg();
-      var currentError = section.querySelector('.form__error');
+      var fieldset = sections[this.currentIndex].querySelector('.form__fieldset');
+      if (!fieldset) {
+        fieldset = sections[this.currentIndex];
+      }
+      var currentError = fieldset.querySelector('.form__error');
       if (currentError) {
-        section.removeChild(currentError);
+        fieldset.removeChild(currentError);
       }
 
-      var inputs = section.querySelectorAll('.js-input');
+      // If only 1 input came, not nodelist
+      if(!inputs.length) {
+        var arr = [];
+        arr.push(inputs);
+        inputs = arr;
+      }
 
       for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].type === 'radio' && inputs[i].checked) {
-          return inputs[i];
-        }
-        if ((inputs[i].type === 'text' || inputs[i].type === 'date'
-        || inputs[i].type === 'email') && inputs[i].value) {
+        var inputType = inputs[i].type;
+        var inputValue = inputs[i].value;
+        var inputNodeName = inputs[i].nodeName;
+
+        if (inputType === 'radio' && inputs[i].checked) {
           return inputs[i];
         }
 
-        if (inputs[i].nodeName === 'SELECT' && inputs[i].selectedOptions[0].value) {
+        if ((inputType === 'text' || inputType === 'date'
+        || inputType=== 'email' || inputType === 'tel') && inputValue) {
+          return inputs[i];
+        }
+
+        if (inputNodeName === 'SELECT' && inputs[i].selectedOptions[0].value) {
           return inputs[i];
         }
       }
 
-      section.appendChild(errorMessage);
+      fieldset.appendChild(errorMessage);
       return false;
     },
 
@@ -135,17 +153,13 @@
 
       var i = this.currentIndex;
 
-      console.log('this.currentIndex: ' + this.currentIndex);
-      console.log('this.nextIndex: ' + this.nextIndex);
-
-      console.log('this.clickFromResult: ' + this.clickFromResult);
-
       if (i === 0 && appNavigation.classList.contains('u-hidden')) {
         this.showNavigation();
       }
 
       if (i === sections.length - 2) {
         this.goToFinalSection();
+        return false;
       }
 
       this.hideCurrentSection();
@@ -154,12 +168,11 @@
         this.nextIndex = this.currentIndex + 1;
       } else {
         this.clickFromResult = false;
-        // this.currentIndex = this.nextIndex;
       }
 
       this.showSection(this.nextIndex);
-
       this.currentIndex = this.nextIndex;
+      this.getStorageValue();
     },
 
     hideCurrentSection: function () {
@@ -178,9 +191,9 @@
       this.currentIndex = index;
 
       // If current section has not passed validation, return to it on next hop
-      if (!this.validateInputs(sections[this.currentIndex])) {
-        this.nextIndex = this.currentIndex;
-      }
+      // if (!this.validateInputs(sections[this.currentIndex])) {
+      //   this.nextIndex = this.currentIndex;
+      // }
 
       sections[this.currentIndex].classList.add('u-hidden');
 
@@ -188,11 +201,14 @@
         sections[index].classList.remove('u-hidden');
         sections[index].classList.add('fade-in');
       }, 300);
-
     },
 
     goToFinalSection: function () {
       app.classList.add('fade-out');
+
+      this.hideCurrentSection();
+      this.currentIndex = sections.length - 1;
+      this.showSection(this.currentIndex );
 
       setTimeout(function () {
         June.nextButton.classList.add('u-hidden');
@@ -207,11 +223,19 @@
         app.appendChild(newAppNavigation);
       }, 100);
 
+      var inputs = sections[this.currentIndex].querySelectorAll('.js-input');
+
+      for (var i = 0; i < inputs.length; i++) {
+        June.getStorageValue(inputs[i]);
+      }
+
       setTimeout(function () {
         app.classList.add('fade-in');
         app.classList.remove('fade-out');
         app.classList.remove('u-hidden');
       }, 300);
+
+      this.submitButton.addEventListener('click', June.submitButtonClickHandler);
 
     },
 
@@ -241,9 +265,15 @@
       span.textContent = Active[type];
       button.appendChild(span);
 
-      // if(this.clickFromResult) {
-      //   resultSections[index].querySelector('input').value = input.value;
-      // }
+      // Update current result
+
+      if (this.clickFromResult) {
+        var inputs = sections[this.currentIndex].querySelectorAll('.js-input');
+        var input = this.validateInputs(inputs);
+        if (input) {
+          resultSections[this.currentIndex].querySelector('input').value = input.value;
+        }
+      }
 
       this.activeResult = button;
       this.currentIndex = index;
@@ -277,10 +307,85 @@
         June.setActiveResult(button, index);
         June.goToSection(index);
       });
+    },
+
+    checkStorage: function () {
+      try {
+        this.storage = localStorage.getItem('test');
+      } catch (err) {
+        this.isStorageSupport = false;
+      }
+      return false;
+    },
+
+    setStorageValue: function (input) {
+      if (this.isStorageSupport) {
+        var inputName = input.name;
+        var value = input.value;
+        if (input.nodeName === 'SELECT') {
+          value = input.selectedOptions[0].value;
+        }
+        localStorage.setItem(inputName, value);
+      }
+    },
+
+    getStorageValue: function () {
+      if (this.isStorageSupport) {
+
+        var section = sections[this.currentIndex];
+        var inputs = section.querySelectorAll('.js-input');
+
+        for (var i = 0; i < inputs.length; i++) {
+
+          if (inputs[i].type === 'radio' || inputs[i].type === 'checkbox') {
+            if (localStorage.getItem(inputs[i].name) === inputs[i].value) {
+              inputs[i].checked = true;
+            }
+          } else if (inputs[i].nodeName === 'SELECT') {
+            if (localStorage.getItem(inputs[i].name)) {
+              var options = inputs[i].options;
+
+              for (var j = 0; j < options.length; j++) {
+                if (localStorage.getItem(inputs[i].name) === options[j].value) {
+                  inputs[i].selectedIndex = j;
+                }
+              }
+            }
+          } else {
+            if (localStorage.getItem(inputs[i].name)) {
+              inputs[i].value = localStorage.getItem(inputs[i].name);
+            }
+          }
+        }
+      }
+    },
+    submitButton: app.querySelector('.js-btn-submit'),
+
+    submitButtonClickHandler: function (evt) {
+      evt.preventDefault();
+      var inputs = sections[June.currentIndex].querySelectorAll('.js-input');
+
+      for (var i = 0; i < inputs.length; i++) {
+        if (June.validateInputs(inputs[i])) {
+          inputs[i].classList.remove('form__input--invalid');
+          June.setStorageValue(inputs[i]);
+        } else {
+          inputs[i].classList.add('form__input--invalid');
+        }
+      }
+      if(!sections[June.currentIndex].querySelector('.form__input--invalid')) {
+        var form = app.querySelector('#june-form');
+        form.submit();
+        localStorage.clear();
+      }
     }
   };
 
+
   window.onload = function () {
+    June.checkStorage();
+    June.getStorageValue();
+
     June.nextButton.addEventListener('click', June.nextButtonClickHandler);
   };
 
